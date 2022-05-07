031c67e7f36a147f24f96f10a3b7355926cc33c1
Hakan Fıstık <hakam.fstk@gmail.com>
2022-05-07 11:53:03 +0300

    Add CanExecute to the Command

--- MvvmToolkit/MainModel.cs
+++ MvvmToolkit/MainModel.cs
@@ -5,16 +5,21 @@ public partial class MainModel
 {
     [ObservableProperty]
     [AlsoNotifyChangeFor(nameof(FullName))]
+    [AlsoNotifyCanExecuteFor(nameof(SumbitCommand))]
     private string _firstName = string.Empty;
 
     [ObservableProperty]
     [AlsoNotifyChangeFor(nameof(FullName))]
+    [AlsoNotifyCanExecuteFor(nameof(SumbitCommand))]
     private string _lastName = string.Empty;
 
     public string FullName => $"{FirstName} {LastName}";
 
-    [ICommand]
-    public void Sumbit()
+    private bool CanSubmit =>
+        !string.IsNullOrEmpty(FirstName) && !string.IsNullOrEmpty(LastName);
+
+    [ICommand(CanExecute = nameof(CanSubmit))]
+    private void Sumbit()
     {
         FirstName = string.Empty;
         LastName = string.Empty;
